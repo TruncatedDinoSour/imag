3c880fdef1baf0148b323d845ba3ec26fc3106cf
Ari Archer <ari@ari.lt>
2024-04-10 15:50:40 +0300

    2.0.1 : implement better voting handling
    Signed-off-by: Ari Archer <ari@ari.lt>

--- src/imag/static/index.js
+++ src/imag/static/index.js
@@ -4,20 +4,29 @@ function vote(mode, id) {
     fetch(`/vote/${mode}/${id}`, { method: "POST" })
         .then((r) => {
             if (!r.ok) {
-                alert("Failed to vote (keep in mind you can only vote once a day)");
-
-                throw new Error(
-                    `Failed to record the vote: ${r.status}`,
+                alert(
+                    "Failed to vote (keep in mind you can only vote once a day)",
                 );
+
+                throw new Error(`Failed to record the vote: ${r.status}`);
             }
 
-            return r.text();
-        })
-        .then((t) => {
-            history.pushState(null, null, `/#${id}`);
+            fetch(`/api/image/${id}`)
+                .then((r) => {
+                    if (!r.ok) {
+                        alert("Failed to get current vote score");
 
-            document.write(t);
-            document.close();
+                        throw new Error(
+                            `Failed to fetch the vote score: ${r.status}`,
+                        );
+                    }
+
+                    return r.json();
+                })
+                .then((j) => {
+                    document.getElementById(`score-${id}`).innerText = j.score;
+                })
+                .catch((e) => console.error(e));
         })
         .catch((e) => console.error(e));
 }
